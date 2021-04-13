384a75526fb78d5eb53b560f6a79e4b5fea3a4e2
Hannkirchwehm <Hannahkirchwehm@mail.missouri.edu>
2021-04-13 16:48:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 "use strict";
 
-var menuNav= document.getElementById("menu_nav");
+var menuNav = document.getElementById('menu_nav');
 
 var menuTrigger = document.getElementById('menu_triggger');
 
-
-
-menuTrigger.onclick = function(){
+function menuToggle(){
   if (menuNav.style.display === "flex") {
     menuNav.style.display = "none";
   } else {
     menuNav.style.display = "flex";
   }
   console.log("The menu has been toggled!")
+}
+
+menuTriger.onclick = menuToggle;
+
+window.onresize = function() {
+if (window.innerwidth>= 600) {
+  menuNav.removeAttribute("style");
+  } ;
 }
