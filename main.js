bbef82068e89acc0fd0757d61fc31c8d9965c310
Hannkirchwehm <Hannahkirchwehm@mail.missouri.edu>
2021-04-13 18:04:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 "use strict";
 
-var menuNav = document.getElementById('menu_nav');
+var menuNav = document.getElementById("menu_nav");
 
-var menuTrigger = document.getElementById('menu_triggger');
+var menuTrigger = document.getElementById('menu_trigger');
 
-function menuToggle(){
+
+menuTrigger.onclick = function(){
   if (menuNav.style.display === "flex") {
     menuNav.style.display = "none";
   } else {
     menuNav.style.display = "flex";
   }
-  console.log("The menu has been toggled!")
-}
-
-menuTriger.onclick = menuToggle;
-
-window.onresize = function() {
-if (window.innerwidth>= 600) {
-  menuNav.removeAttribute("style");
-  } ;
+  console.log ("The menu has been toggled!")
 }
