07904a665072436ba681b0e17d1b8b1737143f4e
Hannkirchwehm <Hannahkirchwehm@mail.missouri.edu>
2021-04-13 16:32:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 "use strict";
 
 var menuNav= document.getElementById("menu_nav");
+
+var menuTrigger = document.getElementById('menu_triggger');
+
+
+
+menuTrigger.onclick = function(){
+  console.log("Hi");
+}
